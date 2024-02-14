e691313d150189059e05a0fe5108738fadc3ee09
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-02-14 12:55:42 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+
+     print("         ,r\'\"7")
+     print("r`-_   ,'  ,/")
+     print(" \. \". L_r'")
+     print("   `~\/")
+     print("      |")
+     print("      |")
