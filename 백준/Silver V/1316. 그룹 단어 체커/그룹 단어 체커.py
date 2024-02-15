c794b9be59da616a9c797e9c1041a7cf9b473714
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-02-15 14:39:49 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver V/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver V/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -5,24 +5,15 @@ if __name__ == '__main__':
      count = N
 
      for n in range(N):
-          x = sys.stdin.readline().rstrip('\n')
-          y = list(set(x))  # 중복 X
-          sig = 0
-          for i in y:
-               min = x.find(i)
-               max = x.find(i)
-               for j in range(len(x)-1,-1,-1):
-                    if x[j] == i:
-                         max = j
-                         break
+         x = sys.stdin.readline().rstrip('\n')
+         sig = 0
+         for i in range(len(x)-1):
+              if x[i] != x[i+1]:
+                   if x[i] in x[i+1:]:
+                        sig = 1
 
-               for j in range(min,max+1): #값 사이에 다른 문자가 있으면 그룹 단어 X
-                    if i!=x[j]:
-                         count -=1
-                         sig = 1
-                         break
-
-               if sig==1:
-                    break
+              if sig ==1:
+                  count-=1
+                  break
 
      print(count)
