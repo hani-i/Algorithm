39929ed67a97c45f99f58104611036002eaf26d1
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-02-23 15:19:35 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -2,15 +2,4 @@ import sys
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline().rstrip('/n'))
-    ini = 2
-
-    for i in range(N):
-        ini = ini*2 - 1
-
-    print(ini**2)
-
-
-
-
-
-
+    print((2**N+1)**2)
