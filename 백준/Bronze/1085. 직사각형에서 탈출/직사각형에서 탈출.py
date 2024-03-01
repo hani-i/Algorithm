22492ddd079089c94f978d2585ed644e0fb45263
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-03-01 14:19:15 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -3,11 +3,5 @@ import sys
 
 if __name__ == '__main__':
     x, y, w, h = map(int, sys.stdin.readline().split())
-    wx = w - x
-    hy = h - y
 
-    list = [x, y, wx, hy]
-    print(min(list))
-
-
-
+    print(min(x, y, w-x, h-y))
