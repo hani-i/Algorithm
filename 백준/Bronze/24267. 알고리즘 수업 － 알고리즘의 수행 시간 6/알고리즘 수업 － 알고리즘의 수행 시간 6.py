0740adcfb19d8ee2f2690db617bf4371ba6e1221
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-03-05 16:36:08 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.py
@@ -0,0 +1,13 @@
+import sys
+
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    sum = 0
+    for i in range(1, n-1):
+        sum += i*(i+1)//2
+
+    print(sum)
+    print(3)
+
+
