4e79a2c49afa9172f5ca316fc680c22403e19f82
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-03-12 17:56:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,26 +1,22 @@
 import sys
 
-def check(n, x, y):
-    total = 5000
-    for i in range(1, int((n/x)+1)):
-        if (n - (x*i))%y == 0:
-            tmp = i + (n-(x*i))//y
-            if tmp < total:
-                total = tmp
-    return total
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
-    value = []
+    count = 0
 
-    value.append(check(N,3,5))
-    value.append(check(N,5, 3))
-    
-    if value.count(5000) == 2:
+    while 0 < N:
+        if N % 5 == 0:
+            count = count + (N // 5)
+            N = 0
+            break
+        else:
+            N -= 3
+            count +=1
+
+    if (N != 0):
         print(-1)
     else:
-        print(min(value))
-
-
+        print(count)
 
 
