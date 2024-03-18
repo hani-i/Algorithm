65e554bc67a2b22b8685c4efaa2a2a168ed56484
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-03-18 20:49:46 +0900

    [Silver V] Title: 단어 정렬, Time: 88 ms, Memory: 36552 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -4,23 +4,13 @@ import sys
 if __name__ == '__main__':
 
     N = int(sys.stdin.readline())
-    myList = []
+    mySet = set()
 
     for _ in range(N):
-        myList.append(sys.stdin.readline().rstrip())
+        mySet.add(sys.stdin.readline().rstrip())
 
-    idx = 0
-    for i in range(N):
-        if idx == len(myList)-1:
-            break
-        else:
-            if myList.count(myList[idx]) >= 2:
-                myList.remove(myList[idx])
-            else:
-                idx += 1
-
+    myList = list(mySet)
     myList.sort(key=lambda x: (len(x), x))
 
-    for i in range(len(myList)):
-        print(myList[i])
+    print(*myList, sep="\n")
 
