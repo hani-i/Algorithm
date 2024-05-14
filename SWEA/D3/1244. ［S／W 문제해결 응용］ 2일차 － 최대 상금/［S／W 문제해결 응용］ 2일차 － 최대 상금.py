aafe5609eef9276f5c367aa88292b0b03027608c
haneul0509 <121336350+haneul0509@users.noreply.github.com>
2024-05-14 21:33:02 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 163 ms, Memory: 50,436 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,29 +1,32 @@
 
-
-def dfs(x, c):
-    global Mymax
-    #print(f'c={c}, x={x}')
-    if c == count:
-        Mymax = max(Mymax, int("".join(map(str, x))))
-        return
-
-    for i in range(len(x)-1):
-        for j in range(i+1, len(x)):
-            x[i], x[j] = x[j], x[i]
-            intx = int("".join(map(str, x)))
-            myList =[intx, c]
-            if myList not in v: #단순히 값만 같으면 안됨. 교환횟수까지 기록해야 한다.
-                v.append(myList)
-                dfs(x, c+1)
-
-            x[i], x[j] = x[j], x[i] #꼭 다시 원상복구
-
 T = int(input())
+
+#
+def dfs(n):
+    global answer
+    if n == count:
+        cur = "".join(num)
+        cur = int(cur)
+        answer = max(answer, int(cur))
+        return
+    for i in range(len(num)-1):
+        for j in range(i+1,len(num)):
+            num[i],num[j] = num[j],num[i]
+            tmp = int("".join(num))
+
+            if (tmp, n+1) not in v:
+                v[(tmp,n+1)] = True
+                dfs(n + 1)
+
+            num[i],num[j] = num[j], num[i]
+
+
 for t in range(T):
-    num, count = input().split()
-    num = list(map(str, num))
+    answer = 0
+
+    num,count = input().split()
     count = int(count)
-    Mymax = 0 #최댓값 저장
-    v = [] # 확인했던 길은 가지 않도록 가지치기
-    dfs(num,0)
-    print(f'#{t+1} {Mymax}')
+    num = list(num)
+    v = {}
+    dfs(0)
+    print(f'#{t+1} {answer}')
